d71a231bad5104faf307af0b6c425025a16ef62b
Diego-Alejandro-Gonzalez-Vargas <82412798+Diego-Alejandro-Gonzalez-Vargas@users.noreply.github.com>
2023-04-05 15:20:29 -0500

    ProcesoUnoListo

---
+++ casoDos/src/casoDosTodo/Principal.java
@@ -0,0 +1,47 @@
+package casoDosTodo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Principal {
+	private static int filas;
+	private static int cols;
+	private static int bytesInt;
+	private static int sizePag;
+	private static int numMarcos;
+	
+	public static void main(String[] args) throws IOException {
+
+		Principal instancia = new Principal();
+		File archivo = new File("C:/a_semestre_v/INFRACOMP/basura/InfraComp/InfraComp/caso2_jbarrios_sepenuela/casoDos/docs/inicial.txt");
+		
+		
+		try (FileReader lector = new FileReader(archivo); BufferedReader br = new BufferedReader(lector);) {
+			
+			String filasStr = br.readLine();
+			filas = Integer.parseInt(filasStr);
+			
+			String colsStr = br.readLine();
+			cols = Integer.parseInt(colsStr);
+			
+			String bytesIntStr = br.readLine();
+			bytesInt = Integer.parseInt(bytesIntStr);
+			
+			String sizePagStr = br.readLine();
+			sizePag = Integer.parseInt(sizePagStr);
+			
+			String NumMarcosStr = br.readLine();
+			numMarcos = Integer.parseInt(NumMarcosStr);
+
+
+		}
+		ProcesoUno procesoUno = new ProcesoUno(filas, cols, bytesInt, sizePag, numMarcos);
+		procesoUno.hacerTodo();
+	}
+	
+	
+
+}
---
+++ casoDos/src/casoDosTodo/ProcesoUno.java
@@ -0,0 +1,59 @@
+package casoDosTodo;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ProcesoUno {
+	private int filas;
+	private int cols;
+	private int bytesInt;
+	private int sizePag;
+	private int numMarcos;
+	public ProcesoUno(int filas, int cols, int bytesInt, int sizePag, int numMarcos) {
+		this.filas = filas;
+		this.cols = cols;
+		this.bytesInt = bytesInt;
+		this.sizePag = sizePag;
+		this.numMarcos = numMarcos;
+	}
+	
+	public void hacerTodo() throws IOException {
+		File archivo = new File("C:\\a_semestre_v\\INFRACOMP\\basura\\InfraComp\\InfraComp\\caso2_jbarrios_sepenuela\\casoDos\\docs/procesoUno.txt");
+		FileWriter escritor = new FileWriter(archivo);
+		escritor.write("TP="+sizePag +"\n");
+		escritor.write("NF="+filas+"\n");
+		escritor.write("NC="+cols+"\n");
+		int numRefs = filas*cols*3;
+		escritor.write("NR="+numRefs+"\n");
+		int pagina =0;
+		int desplazamiento =0;
+		for(int i =0; i<filas;i++)
+		{
+			for (int j=0; j<cols; j++)
+			{
+				
+				String renglonAa = "[A-"+i+"-"+j+"],"+pagina+", "+desplazamiento+"\n";
+				pagina+= ((desplazamiento+bytesInt)/sizePag);
+				desplazamiento = ((desplazamiento+bytesInt)%sizePag);
+				escritor.write(renglonAa);
+				String renglonBb = "[B-"+i+"-"+j+"],"+pagina+", "+desplazamiento+"\n";
+				pagina+= ((desplazamiento+bytesInt)/sizePag);
+				desplazamiento = ((desplazamiento+bytesInt)%sizePag);
+				escritor.write(renglonBb);
+				String renglonCc = "[C-"+i+"-"+j+"],"+pagina+", "+desplazamiento+"\n";
+				pagina+= ((desplazamiento+bytesInt)/sizePag);
+				desplazamiento = ((desplazamiento+bytesInt)%sizePag);
+				escritor.write(renglonCc);
+			}
+		}
+			
+		
+		
+		
+		escritor.close();
+
+
+	}
+
+}
